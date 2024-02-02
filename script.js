8a27332bcfe49d710135ca0d31c2da16f28aa4ad
Ryan <theryanrojas@gmail.com>
2024-02-02 09:32:37 -0800

    Added feature house with carousel and ready to sell section.

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const fHCarouselBtnRight = document.getElementById("fhCarouselBtnRight")
+    const fHCarouselBtnLeft = document.getElementById("fhCarouselBtnLeft")
+    let count = 0;
 
-document.addEventListener('mouseup', (e) => {
-    if(e.target.classList.contains('pagination-dot')) {
-        const carousel = document.querySelector(".hero-image-carousel");
-        // const index = parseInt(getComputedStyle(carousel).getPropertyValue("--slider-index"));
-        const dots = Array.from(document.querySelectorAll(".pagination-dot"));
-        dots.forEach( div => {
-            div.style.backgroundColor = 'rgba(255, 255, 255, 0.478)';
-        })
-        carousel.style.setProperty("--slider-index", e.target.id)
-        e.target.style.backgroundColor = 'white'
+ 
+    document.addEventListener('mouseup', (e) => {
+        if(e.target.classList.contains('pagination-dot')) {
+            const carousel = document.querySelector(".hero-image-carousel");
+            // const index = parseInt(getComputedStyle(carousel).getPropertyValue("--slider-index"));
+            const dots = Array.from(document.querySelectorAll(".pagination-dot"));
+            dots.forEach( div => {
+                div.style.backgroundColor = 'rgba(255, 255, 255, 0.478)';
+            })
+            carousel.style.setProperty("--slider-index", e.target.id)
+            e.target.style.backgroundColor = 'white'
+    
+        }   
+      });
+    
+    
+      fHCarouselBtnRight.addEventListener('click', () => {
+        count++
+        const sliderIndex = document.querySelector(".feature-house-container")
+        sliderIndex.style.setProperty("--feature-house-slider-index",  count)
+        console.log(sliderIndex)
+      })
 
-        // if(e.target.id == 1) {
-        //     carousel.style.setProperty("--slider-index", 1)
-        // } else if (e.target.id == 2) {
-        //     carousel.style.setProperty("--slider-index", 2)
+      fHCarouselBtnLeft.addEventListener('click', () => {
+        count--
+        const sliderIndex = document.querySelector(".feature-house-container")
+        sliderIndex.style.setProperty("--feature-house-slider-index", count)
+        console.log(sliderIndex)
+      })
+})
 
-        // } else {
-        //     carousel.style.setProperty("--slider-index", 0)
-
-        // }
-
-       
-        // if (index <= 1) {
-        //     console.log(index)
-        //     carousel.style.setProperty("--slider-index", index + 1)
-        // } else {
-        //     carousel.style.setProperty("--slider-index", index - 2)
-        // }
-    }   
-  });
-  
